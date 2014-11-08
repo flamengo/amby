6b14cb8192f89e79455f2e245566804b3c97d17c
flamengo <pattaragorn.kh@gmail.com>
2014-11-08 21:15:41 +0700

    reform library at head

--- view/lib.php
+++ view/lib.php
@@ -1,6 +1,7 @@
 <head>
-	<script src="<? echo base_url('assets/js/jquery-1.11.1.min.js'); ?>" type="text/javascript"></script>
-	<script src="<? echo base_url('assets/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
-	<link href="<? echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" charset="UTF-8">
+	<script src="/assets/js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
 
