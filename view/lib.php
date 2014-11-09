bf897310821582b1cfb05cf037aa9071986b7ffa
flamengo <pattaragorn.kh@gmail.com>
2014-11-09 21:07:47 +0700

    use google's font

--- view/lib.php
+++ view/lib.php
@@ -3,5 +3,6 @@
 	<script src="/assets/js/jquery-1.11.1.min.js" type="text/javascript"></script>
 	<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
 	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Kalam:400,700' rel='stylesheet' type='text/css'>
 </head>
 
